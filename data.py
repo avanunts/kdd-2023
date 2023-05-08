f8b0e97e599bc39169bdb891241aa7a12a90b561
Arsenii Vanunts <ar16sen@gmail.com>
2023-05-08 19:15:39 +0200

    First trial commit

---
+++ data.py
@@ -0,0 +1,6 @@
+import os
+
+data_dir = os.path.join("/", "Users", "fridrihkakiev", "Downloads", "kdd 2023 datasets")
+sessions_train_path = os.path.join(data_dir, "sessions_train.csv")
+products_train_path = os.path.join(data_dir, "products_train.csv")
+sessions_test_path = os.path.join(data_dir, "sessions_test_task1.csv")
